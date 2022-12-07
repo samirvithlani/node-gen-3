0f86f20e69f99737b1a573ada95b8df7a2da08cc
samirvithlani <samir.vithlani83955@gmail.com>
2022-12-07 20:54:49 +0530

    crud

--- learning/controller/UserController.js
+++ learning/controller/UserController.js
@@ -1,48 +1,135 @@
-
 const userSchema = require("../model/UsersSchema");
 
-exports.createUser =(req,res)=>{
-    //req.body > save
-    const user = new userSchema(req.body);
-    //insert --> save
-    user.save((err,data)=>{
+exports.createUser = (req, res) => {
+  //req.body > save
+  const user = new userSchema(req.body);
+  //insert --> save
+  user.save((err, data) => {
+    if (err) {
+      res.status(500).json({
+        message: "Error in saving data",
+      });
+    } else {
+      res.status(201).json({
+        data: data,
+        message: "Data saved successfully",
+      });
+    }
+  });
+};
 
-        if(err){
+exports.getAllUsers = (req, res) => {
+  userSchema.find((err, data) => {
+    if (err) {
+      res.status(500).json({
+        message: "Error in fetching data",
+      });
+    } else {
+      res.status(200).json({
+        data: data,
+        message: "Data fetched successfully",
+      });
+    }
+  });
+};
+exports.deleteUser = (req, res) => {
+  //req.params.id
+  const id = req.params.id;
 
-            res.status(500).json({
-                message:"Error in saving data",  
-            })
+  userSchema.findByIdAndDelete(id, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        message: "Error in deleting data",
+      });
+    } else {
+      if (data != null || data != undefined) {
+        res.status(200).json({
+          data: data,
+          message: "Data deleted successfully",
+        });
+      } else {
+        res.status(404).json({
+          message: "Data not found",
+        });
+      }
+    }
+  });
+};
 
-        }
-        else{
+exports.updateUser = (req, res) => {
+  //id record....
+  //id from url
+  console.log(req.body.name);
+  if (
+    req.body.name == undefined ||
+    req.body.email == undefined ||
+    req.body.password == undefined ||
+    req.body.age == undefined ||
+    req.body.isMarried == undefined
+  ) {
+    console.log("Bad request");
+    res.status(400).json({
+      message: "Bad request",
+    });
+  } else {
+    //if
+    var user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      age: req.body.age,
+      isMarried: req.body.isMarried,
+    };
 
-            res.status(201).json({
-                data:data,
-                message:"Data saved successfully"
-            })
-
-        }
-    })
-}
-
-exports.getAllUsers = (req,res)=>{
-
-    userSchema.find((err,data)=>{
-        if(err){
-            res.status(500).json({
-                message:"Error in fetching data"
-            })
-
-        }
-        else{
+    const id = req.params.id;
+    userSchema.findByIdAndUpdate(id, req.body, (err, data) => {
+      if (err) {
+        res.status(500).json({
+          message: "Error in updating data",
+        });
+      } else {
+        if(data!=null || data!=undefined){
             res.status(200).json({
-                data:data,
-                message:"Data fetched successfully"
-            })
+                data: data,
+                message: "Data updated successfully",
+            });
         }
+        else{
+            res.status(404).json({
+                message: "Data not found",
+            });
+        }
+      }
+    });
+  }
+};
 
+exports.getuserById = (req,res)=>{
 
-    })
+        //its not a rule ... id
+        const id = req.params.id
+        userSchema.findById(id,(err,data)=>{
+            if(err){
+                res.status(500).json({
+                    message:"Error in fetching data",
+                })
+            }
+            else{
+
+                if(data!=null|| data!=undefined){
+                    res.status(200).json({
+                        message:"Data fetched successfully",
+                        //data:data
+                        user:data
+                    })
+                }
+                else{
+                    res.status(404).json({
+                        message:"Data not found",
+                    })
+                }
+            }
+        })
 
 
 }
--- learning/routes/UserRoutes.js
+++ learning/routes/UserRoutes.js
@@ -5,6 +5,8 @@ const userController = require("../controller/UserController");
 
 router.post('/user',userController.createUser)
 router.get('/user',userController.getAllUsers)
-
+router.delete('/user/:id',userController.deleteUser)
+router.put('/user/:id',userController.updateUser)
+router.get('/user/:id',userController.getuserById)
 
 module.exports = router;
